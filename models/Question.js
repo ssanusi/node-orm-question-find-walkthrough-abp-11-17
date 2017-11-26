ce26536adb017b30db99842598eb74d373197280
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-26 07:50:18 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -7,10 +7,10 @@ class Question{
         id INTEGER PRIMARY KEY,
         content TEXT
       )`
-      
+
       db.run(sql, function(){
         resolve("questions table created")
-      })      
+      })
     })
   }
 
@@ -19,12 +19,23 @@ class Question{
   }
 
   insert(){
-    const self = this 
+    const self = this
     const sql = `INSERT INTO questions (content) VALUES (?)`
     return new Promise(function(resolve){
       db.run(sql, [self.content], function(err, result){
         self.id = this.lastID
-        resolve(self)      
+        resolve(self)
+      })
+    })
+  }
+
+  static Find(id){
+    const sql = `SELECT * WHERE id = ? LIMIT 1`,
+    return new Promise(function(resolve){
+      db.get(sql, [id] , function(err,resultRow){
+        const question = new Question(resultRow.content);
+        question.id = resultRow.id;
+        resolve(question);
       })
     })
   }
